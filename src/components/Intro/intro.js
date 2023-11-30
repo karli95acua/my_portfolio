b654f8beb58ae8ef6eb8933d3747406567162101
karli95acua <karli.a.mella@gmail.com>
2023-11-30 15:52:22 -0300

    se agrega cv disponible para descarga

--- src/components/Intro/intro.js
+++ src/components/Intro/intro.js
@@ -3,6 +3,7 @@ import './intro.css';
 import { Link } from 'react-scroll';
 import contactImg from '../../assets/contact.png';
 import bg from '../../assets/introimg.png';
+import CvPdf from '../../assets/CV_KARLA_A_2023:S-EN.pdf';
 
 
 const Intro = () => {
@@ -13,9 +14,13 @@ const Intro = () => {
                 <span className="introText"> Soy <span className="introName"> Karli Acuña</span> <br />Full Stack Developer</span>
                 <p className="introPara">
                     Soy una desarrolladora web,
-                    apasionada por la programación y <br/> la creación de proyectos atractivos y con buen desempeño.</p>
-                <Link to="contact" spy={true} smooth={true} offset={-100} duration={500}>
-                    <button className="btn"><img src={contactImg} alt="contactImg" className="btnImg"/>Contáctame!</button></Link>
+                    apasionada por la programación y <br/> la creación de proyectos atractivos y con buen desempeño.</p> 
+                    <Link to="contact" spy={true} smooth={true} offset={-100} duration={500}>
+                        <button className="btn"><img src={contactImg} alt="contactImg" className="btnImg"/>Contáctame!</button>
+                    </Link>
+                    <a href={CvPdf} download>
+                        <button className="btn2">Descarga mi CV</button>
+                    </a> 
             </div>
             <img src={bg} alt="Profile" className="bg"/>
         </section>
