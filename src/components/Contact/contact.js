1550e7dc7ce782a84ee2ae0d4a3ef4d6d0990943
karli95acua <karli.a.mella@gmail.com>
2023-11-30 00:48:05 -0300

    mejoras en servicio de correo

--- src/components/Contact/contact.js
+++ src/components/Contact/contact.js
@@ -5,9 +5,15 @@ import emailjs from '@emailjs/browser';
 
 const Contact = () => {
     const form = useRef();
+
     const sendEmail = (e) => {
         e.preventDefault();
-    
+
+        if (!form.current.elements.from_email.value) {
+            alert('Por favor, ingresa tu correo electrónico.');
+            return;
+        }
+
         emailjs.sendForm('service_s2sxxvn', 'template_5mwb0ps', form.current, 'YRYuaperHQwFanGoG')
         .then((result) => {
             console.log(result.text);
@@ -15,17 +21,19 @@ const Contact = () => {
             alert('Email enviado con éxito');
         }, (error) => {
             console.log(error.text);
+            alert('Hubo un error al enviar el correo, inténtalo de nuevo.');
         });
     };
+
     return (
         <section id='contactPage'>
             <div id='contact'>
                 <h1 className='contactPageTitle'> Contáctame</h1>
                 <span className='contactDesc'> Por favor llena este formulario para mantenernos en contacto:</span>
                 <form className='contactForm' ref={form} onSubmit={sendEmail}>
-                    <input type='text' className='name' placeholder='Tu nombre completo' name='from_name' />
-                    <input type='email' className='email' placeholder='Tu email' name='from_email'/>
-                    <textarea name='message' className='msg' rows='5' placeholder='Tu mensaje'></textarea>
+                    <input type='text' className='name' placeholder='Tu nombre completo' name='from_name' required />
+                    <input type='email' className='email' placeholder='Tu email' name='from_email' required />
+                    <textarea name='message' className='msg' rows='5' placeholder='Tu mensaje' required></textarea>
                     <button type='submit' value='send' className='submitBtn'>Enviar</button>
                     <div className='links'>
                         <a href='https://www.linkedin.com/in/karli-acuna/' target='_blank' rel='noopener noreferrer'>
@@ -36,6 +44,6 @@ const Contact = () => {
             </div>
         </section>
     );
-}
+};
 
-export default Contact
+export default Contact;
